1da8fa387b4b9dc4a44c975cdad1473e34c210c3
555Viktor <victor.vaskov2002@gmail.com>
2024-12-16 10:29:03 +0200

    Create JS dom variables

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pager = pages;
-    this.isRead = isRead;
-}
+// DOM Elements
+const bookNameInput = document.querySelector('.input-book-name');
+const bookAuthorInput = document.querySelector('.input-author');
+const bookPagesInput = document.querySelector('.input-pages');
+const bookStatusSelect = document.querySelector('.input-read-status');
+const addBookButton = document.querySelector('.btn-add-book');
+
+const readStatusButton = document.querySelector('.btn-read-status');
+const deleteBookButton = document.querySelector('.btn-delete-book');
