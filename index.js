60810109897b873bc4155c7e82796aabab03dc39
555Viktor <victor.vaskov2002@gmail.com>
2024-12-16 10:53:34 +0200

    Create object constructor, import CSS font

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const bookNameInput = document.querySelector('.input-book-name');
 const bookAuthorInput = document.querySelector('.input-author');
 const bookPagesInput = document.querySelector('.input-pages');
 const bookStatusSelect = document.querySelector('.input-read-status');
-const addBookButton = document.querySelector('.btn-add-book');
 
+const addBookButton = document.querySelector('.btn-add-book');
 const readStatusButton = document.querySelector('.btn-read-status');
 const deleteBookButton = document.querySelector('.btn-delete-book');
+
+let myBooks = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead || false;
+}
+
+addBookButton.addEventListener('click', () => {
+    console.log('Book Name:', bookNameInput.value);
+    console.log('Author:', bookAuthorInput.value);
+    console.log('Pages:', bookPagesInput.value);
+    console.log('Read Status:', bookStatusSelect.value);
+})
