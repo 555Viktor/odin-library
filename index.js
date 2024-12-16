f1bba93149b9e092aad55bf863ba306041d51625
555Viktor <victor.vaskov2002@gmail.com>
2024-12-16 10:57:02 +0200

    Fix DOM variables, test event listener

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // DOM Elements
-const bookNameInput = document.querySelector('.input-book-name');
-const bookAuthorInput = document.querySelector('.input-author');
-const bookPagesInput = document.querySelector('.input-pages');
-const bookStatusSelect = document.querySelector('.input-read-status');
+const bookNameInput = document.querySelector('.input-field #input-book-name');
+const bookAuthorInput = document.querySelector('.input-field #input-author');
+const bookPagesInput = document.querySelector('.input-field #input-pages');
+const bookStatusSelect = document.querySelector('.input-field #input-read-status');
 
 const addBookButton = document.querySelector('.btn-add-book');
 const readStatusButton = document.querySelector('.btn-read-status');
